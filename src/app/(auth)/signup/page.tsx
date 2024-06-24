4d3c7a8db7263364073285826ee5b7bbdd24069c
YoungUnKim <162089313+YoungUnKim@users.noreply.github.com>
2024-06-25 00:15:08 +0900

    ✨ feat : 회원가입 기본 형태 제작

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,5 +1,31 @@
+import Link from "next/link";
+
 function SignUp() {
-  return <div>signup</div>;
+  return (
+    <form>
+      <div>회원가입</div>
+      <div>
+        <label htmlFor="name">이름</label>
+        <input id="name" placeholder="이름을 입력해주세요" />
+      </div>
+      <div>
+        <label htmlFor="email">이메일</label>
+        <input id="email" placeholder="이메일을 입력해주세요" />
+      </div>
+      <div>
+        <label htmlFor="password">비밀번호</label>
+        <input id="password" type="password" placeholder="비밀번호를 입력해주세요" />
+      </div>
+      <div>
+        <label htmlFor="password_confirmation">비밀번호 확인</label>
+        <input id="password_confirmation" placeholder="비밀번호를 입력해주세요" />
+      </div>
+      <button type="submit">가입하기</button>
+      <p>
+        이미 회원이신가요?<Link href="./login">로그인하기</Link>
+      </p>
+    </form>
+  );
 }
 
 export default SignUp;
