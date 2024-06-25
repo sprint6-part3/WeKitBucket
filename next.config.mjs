bedf9b48f6ff91d8579bef742cea73fe3ebf2166
eunbinnie <dmsqlsdl54@naver.com>
2024-06-25 18:33:39 +0900

    ✨ fix(404): 404 페이지 퍼블리싱 완료

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,15 @@ const nextConfig = {
   swcMinify: true,
   webpack(config) {
     config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ["@svgr/webpack"],
+      test: /\.svg$/,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            icon: true,
+          },
+        },
+      ],
     });
 
     return config;
