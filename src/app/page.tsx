b329b864bf7e98b1210b3f967da3856c7c403045
Darkthos <kiki7951@naver.com>
2024-06-27 00:41:43 +0900

    ✨ fix(home)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,23 +2,23 @@
 // import getProfiles from "@/apis/profile/getProfiles";
 // import { UserInput } from "@/types/auth";
 
-// function Home() {
-//   const user: UserInput = {
-//     email: "nape1203@gmail.com",
-//     password: "im79517951",
-//   };
+function Home() {
+  //   const user: UserInput = {
+  //     email: "nape1203@gmail.com",
+  //     password: "im79517951",
+  //   };
 
-//   const load = async () => {
-//     const { list } = await getProfiles({ page: 1, name: "김" });
-//     console.log(list);
-//   };
-//   load();
+  //   const load = async () => {
+  //     const { list } = await getProfiles({ page: 1, name: "김" });
+  //     console.log(list);
+  //   };
+  //   load();
 
-//   const load = async () => {
-//     const list = await postSignIn(user);
-//     console.log(list);
-//   };
-//   load();
+  //   const load = async () => {
+  //     const list = await postSignIn(user);
+  //     console.log(list);
+  //   };
+  //   load();
 
   return (
     <div>
