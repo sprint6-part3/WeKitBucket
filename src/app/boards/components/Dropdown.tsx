72a4233fb8aba421906e7675731545d2ae5702f7
eunbinnie <dmsqlsdl54@naver.com>
2024-06-28 19:43:51 +0900

    ✨feat(sort): 게시글 정렬 기능 작업 완료

--- src/app/boards/components/Dropdown.tsx
+++ src/app/boards/components/Dropdown.tsx
@@ -5,12 +5,32 @@
 import React, { useState } from "react";
 import Caret from "@/assets/icons/caret.svg";
 
-function Dropdown() {
-  const sort = ["최신순", "좋아요순"];
+export interface ISortValue {
+  text: string;
+  id: "recent" | "like";
+}
+
+interface IDropdownProps {
+  sortValue: ISortValue[];
+  onClick: (id: "recent" | "like") => void;
+}
+
+function Dropdown({ sortValue, onClick }: IDropdownProps) {
   const [view, setView] = useState(false);
+  const [selected, setSelected] = useState(sortValue[0].text);
 
   const toggleDropdown = () => {
     setView(!view);
+  };
+
+  const onClickSort = (id: "recent" | "like") => () => {
+    const selectedValue = sortValue.find(value => value.id === id);
+    if (selectedValue) {
+      setSelected(selectedValue.text);
+    }
+
+    toggleDropdown();
+    onClick(id);
   };
 
   return (
@@ -20,19 +40,21 @@ function Dropdown() {
         onClick={toggleDropdown}
         className="flex w-full cursor-pointer items-center justify-between rounded-[10px] bg-primary-gray-100 px-5 py-[10.5px]"
       >
-        <p className="text-sm leading-[1.7] text-primary-gray-500">{sort[0]}</p>
+        <p className="text-sm leading-[1.7] text-primary-gray-500">{selected}</p>
         <Caret width={7} height={4} />
       </button>
       {view && (
         <>
           <div className="fixed bottom-0 left-0 right-0 top-0" onClick={toggleDropdown} />
           <ul className="absolute mt-2 grid w-full gap-[5px] rounded-[10px] bg-white px-1 py-[6px] shadow-custom-shadow">
-            {sort.map(value => (
+            {sortValue.map(sort => (
+              // eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions
               <li
-                key={value}
+                key={sort.id}
+                onClick={onClickSort(sort.id)}
                 className="cursor-pointer px-4 py-[6px] text-sm leading-[1.7] text-primary-gray-500 hover:bg-primary-green-100"
               >
-                {value}
+                {sort.text}
               </li>
             ))}
           </ul>
--- src/app/boards/components/allArticles/AllArticleSection.tsx
+++ src/app/boards/components/allArticles/AllArticleSection.tsx
@@ -1,54 +1,72 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { IArticleProps } from "@/types/articles";
 import getArticles, { ArticleOption } from "@/apis/article/getArticles";
 import SearchForm from "../search/SearchForm";
-import Dropdown from "../Dropdown";
+import Dropdown, { ISortValue } from "../Dropdown";
 import PostHeader from "./PostHeader";
 import PostList from "./PostList";
 
 function AllArticleSection({ article }: IArticleProps) {
   const [articles, setArticles] = useState(article?.list || []);
+  const [keyword, setKeyword] = useState("");
   const [options, setOptions] = useState<ArticleOption>({
     page: 1,
     pageSize: 10,
     orderBy: "recent",
     keyword: "",
   });
+  const sortValue: ISortValue[] = [
+    {
+      text: "최신순",
+      id: "recent",
+    },
+    {
+      text: "좋아요순",
+      id: "like",
+    },
+  ];
 
-  /**
-   * 검색 기능 handle 함수
-   */
-  // const handleSort = () => {};
+  const handleSort = (id: "recent" | "like") => {
+    setOptions(prev => ({
+      ...prev,
+      orderBy: `${id}`,
+    }));
+  };
 
   const handleSearchSubmit: React.FormEventHandler<HTMLFormElement> = async e => {
     e.preventDefault();
-
-    try {
-      const res = await getArticles(options);
-      setArticles(res?.list);
-    } catch (error) {
-      console.error("Failed to fetch getArticles: ", error);
-      // eslint-disable-next-line no-alert
-      alert("죄송합니다, 현재 검색을 처리할 수 없습니다. 잠시 후 다시 시도해 주세요.");
-    }
+    setOptions(prev => ({
+      ...prev,
+      keyword: `${keyword}`,
+    }));
   };
 
   const handleChangeSearchInput: React.ChangeEventHandler<HTMLInputElement> = e => {
     const { value } = e.target;
-    setOptions(prev => ({
-      ...prev,
-      keyword: `${value}`,
-    }));
+    setKeyword(value);
   };
+
+  useEffect(() => {
+    const getArticlesWithOptions = async () => {
+      try {
+        const res = await getArticles(options);
+        setArticles(res?.list);
+      } catch (error) {
+        console.error("Failed to fetch getArticles: ", error);
+      }
+    };
+
+    getArticlesWithOptions();
+  }, [options]);
 
   return (
     <section className="mx-auto grid w-full max-w-[1180px] gap-[30px] px-5 sm:gap-5 sm:px-[60px] md:gap-5">
       {/* 검색 및 정렬 영역 */}
       <div className="flex flex-col items-center justify-center gap-5 md:flex-row">
-        <SearchForm onSubmit={handleSearchSubmit} onChange={handleChangeSearchInput} value={options.keyword} />
-        <Dropdown />
+        <SearchForm onSubmit={handleSearchSubmit} onChange={handleChangeSearchInput} value={keyword} />
+        <Dropdown sortValue={sortValue} onClick={handleSort} />
       </div>
       {/* 리스트 영역 */}
       <div className="w-full">
