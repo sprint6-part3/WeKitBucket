18c3713784d3f78ca37fe18403404aa03148db4d
kimminjae <162538553+PixeIDark@users.noreply.github.com>
2024-07-04 18:49:18 +0900

    ✨ feat/apis: interceptor추가 (#25)
    * ✨ feat(.env): BASE_URL 환경변수 등록
    
    * 🐛 fix(eslint): import시, .ts붙여야하는 규칙 수정
    
    * ✨ feat(type auth file): 유저정보에 관한 타입파일 추가
    
    * 🐛 fix(lang=Ko)
    
    * ✨ feat(signin-up api, instance): 회원가입, 로그인 api구현, 기본 api 유틸함수 추가
    
    * ✨ feat(getarticles, getprofiles): 각 타입파일, get요청 api, Instance개선, 파람유틸함수 생성
    
    * merger
    
    * 🔨 refactor(api): 모든 fetchInstance의 범용성을 높임
    
    * ✨ fix(home)
    
    * fix(get cookies): 쿠키에러가 뜨지않게 instance를 서버 컴포넌트라 명시해줬습니다.
    
    * refactor(login): 쿠키 서버액션을해서 Form 컴포넌트 분리
    
    * 🔀 Merge branch 'main' of https://github.com/Melffys/WeKitBucket into refactor/login
    
    * 🐛 fix: SignUpForm import경로수정
    
    * ✨ feat(apis): api 거의다 추가
    
    * ✨ feat(모든api구현, getArticles고침, instance 범용성 업)
    
    * 🐛 fix: Auth폴더 이름잠시 변경
    
    * 🐛 fix: auth로 다시 변경
    
    * 🐛 fix(testPage): 빌드 실패해서 주석 처리 했습니다.
    
    * ✨ feat(fetchInstance):  add interceptor

--- src/apis/article/getArticles.ts
+++ src/apis/article/getArticles.ts
@@ -2,7 +2,6 @@ import { ArticleData } from "@/types/articles";
 import fetchInstance from "@/utils/fetchInstance";
 
 // 자유게시판 페이지
-
 const getArticles = async (options?: {
   page?: number;
   pageSize?: number;
@@ -13,7 +12,7 @@ const getArticles = async (options?: {
     const data = await fetchInstance<ArticleData>("articles", {
       method: "GET",
       params: options,
-    });
+      
     return data;
   } catch (error) {
     if (error instanceof Error) {
@@ -25,3 +24,4 @@ const getArticles = async (options?: {
 };
 
 export default getArticles;
+
