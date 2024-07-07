add3e40dedf9c60711935be7e7ba86d9d79327c9
eunbinnie <dmsqlsdl54@naver.com>
2024-07-07 15:55:20 +0900

    🔨refcator(페이지 전환): 페이지 전환 애니메이션 수정

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 export default function Template({ children }: { children: React.ReactNode }) {
   return (
     <motion.div
-      initial={{ opacity: 0, scale: 0.985 }}
+      initial={{ opacity: 0, scale: 0.99 }}
       animate={{ opacity: 1, scale: 1 }}
       transition={{ ease: "easeInOut", duration: 0.75 }}
     >
