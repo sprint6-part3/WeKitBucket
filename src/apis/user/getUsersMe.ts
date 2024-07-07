51d68cf525da0c242e7da03acedd3b04d8814d7d
kimminjae <162538553+PixeIDark@users.noreply.github.com>
2024-07-07 17:59:46 +0900

    🐛 fix(getUsersMe): 401에러 발생시킴 => 리프레쉬 토큰있으면 로그인 유지됨 (#45)
    🐛 fix(getUsersMe): 401에러 발생시킴

--- src/apis/user/getUsersMe.ts
+++ src/apis/user/getUsersMe.ts
@@ -6,20 +6,20 @@ import { cookies } from "next/headers";
 
 const getUsersMe = async () => {
   try {
-    if (cookies().get("accessToken")) {
-      const data = await fetchInstance<IUser>("users/me", {
-        method: "GET",
-      });
-      return data;
-    }
-
-    return null;
+    const data = await fetchInstance<IUser>("users/me", {
+      method: "GET",
+    });
+    return data;
   } catch (error) {
+    if (cookies().get("refreshToken")) {
+      console.log("401 Unauthorized - Refresh token only");
+      throw new Error("401 Unauthorized");
+    }
     if (error instanceof Error) {
       console.error(error);
-      throw new Error(error.message || "Artist list failed");
+      throw new Error(error.message || "User data load failed");
     } else {
-      throw new Error("Artist list failed");
+      throw new Error("User data load failed");
     }
   }
 };
