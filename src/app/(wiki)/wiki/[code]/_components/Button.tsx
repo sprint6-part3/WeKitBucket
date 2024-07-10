e88c262bb8005a9bcdce457e37997a3a4386df90
YoungUnKim <162089313+YoungUnKim@users.noreply.github.com>
2024-07-10 21:16:57 +0900

    ✨ feat: 기본 구조 화면 및 퀴즈 모달 추가

--- src/app/(wiki)/wiki/[code]/_components/Button.tsx
+++ src/app/(wiki)/wiki/[code]/_components/Button.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 
-function Button({ type, children }: React.ButtonHTMLAttributes<HTMLButtonElement>) {
+function Button({ type, children, onClick }: React.ButtonHTMLAttributes<HTMLButtonElement>) {
   return (
     <button
       type={type}
+      onClick={onClick}
       className="mt-3 max-w-[400px] rounded-[10px] bg-primary-green-200 px-5 py-[11px] text-sm font-semibold leading-[1.7] text-white"
     >
       {children}
--- src/app/(wiki)/wiki/[code]/_components/Info.tsx
+++ src/app/(wiki)/wiki/[code]/_components/Info.tsx
@@ -24,19 +24,19 @@ const labels: Record<keyof InfoType, string> = {
 function Info({ userData, image, toggleActive, isToggleActive }: ProfileCardProps) {
   return (
     <section
-      className={`flex flex-col rounded-xl border-none px-5 py-3 shadow-lg shadow-gray-300 transition-all duration-300 ease-in-out md:px-5 md:py-6 xl:h-[671px] xl:max-w-[320px] xl:p-[30px] ${isToggleActive ? "h-[260px] md:h-[290px]" : "h-[120px] md:h-[135px]"}`}
+      className={`flex flex-col rounded-xl border-none px-5 py-3 shadow-lg shadow-gray-300 transition-all duration-300 ease-in-out md:px-5 md:py-6 xl:h-[671px] xl:max-w-[320px] xl:p-[30px] ${isToggleActive ? "h-[300px] md:h-[320px]" : "h-[120px] md:h-[145px]"}`}
     >
       <article className="flex gap-5 md:gap-8 xl:flex-col xl:gap-[30px]">
         <div className="relative h-[62px] w-[62px] overflow-hidden rounded-full md:h-[71px] md:w-[71px] xl:m-[30px] xl:h-[200px] xl:w-[200px]">
-          <Image layout="fill" className="object-contain" src={image || UserDafalutImage} alt="프로필이미지" />
+          <Image layout="fill" className="object-contain" src={image || UserDafalutImage} alt="프로필" />
         </div>
 
         <div
-          className={`flex flex-col gap-2 truncate transition-all duration-300 ease-in-out xl:h-[304px] xl:gap-4 ${isToggleActive ? "h-[250px] md:h-[234px]" : "h-[70px] md:h-[80px]"}`}
+          className={`flex flex-col gap-2 truncate transition-all duration-300 ease-in-out xl:h-[304px] xl:gap-4 ${isToggleActive ? "h-[250px] md:h-[255px]" : "h-[85px] md:h-[90px]"}`}
         >
           {Object.entries(userData).map(([key, value]) => (
             <div key={key} className="flex gap-5">
-              <div className="text-12 md:text-14 text-gray-400">{labels[key as keyof InfoType]}</div>
+              <div className="text-12 md:text-14 w-20 text-gray-400">{labels[key as keyof InfoType]}</div>
               <div className="text-12 md:text-14 truncate text-gray-800">{value.length > 0 ? value : "???"}</div>
             </div>
           ))}
@@ -47,7 +47,7 @@ function Info({ userData, image, toggleActive, isToggleActive }: ProfileCardProp
           width={24}
           height={24}
           className={`transform transition-transform duration-300 ease-in-out ${isToggleActive ? "rotate-180" : ""}`}
-          alt={`${isToggleActive ? "프로필 상세 닫기" : "프로필 상세 보기"}`}
+          alt={`${isToggleActive ? "접기" : "펼치기"}`}
         />
       </button>
     </section>
