a925c8867a77dc43e211aa8c874fcc2f0641ce1c
eunbinnie <dmsqlsdl54@naver.com>
2024-07-11 23:10:13 +0900

    🐛fix: 베스트 게시글 get api 코드 삭제

--- src/app/(boards)/boards/components/bestArticles/BestPostList.tsx
+++ src/app/(boards)/boards/components/bestArticles/BestPostList.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import useWindowSize from "@/hooks/useWindowSize";
-import React, { useEffect, useState } from "react";
-import getArticles from "@/apis/article/getArticles";
 import { IArticleProps } from "@/types/articles.type";
 import BestSwiper from "./BestSwiper";
 import BestFlex from "./BestFlex";
@@ -10,27 +8,10 @@ import BestFlex from "./BestFlex";
 function BestPostList({ article }: IArticleProps) {
   const windowSize = useWindowSize();
   const isMobile = windowSize < 640 && windowSize !== 0;
-  const [articles, setArticles] = useState(article);
-
-  useEffect(() => {
-    const getArticlesWithOptions = async () => {
-      try {
-        const res = await getArticles({
-          pageSize: 4,
-          orderBy: "like",
-        });
-        setArticles(res);
-      } catch (error) {
-        console.error("Failed to fetch getArticles: ", error);
-      }
-    };
-
-    getArticlesWithOptions();
-  }, []);
 
   return (
     <div className="w-full overflow-hidden">
-      {isMobile ? <BestSwiper article={articles} /> : <BestFlex article={articles} />}
+      {isMobile ? <BestSwiper article={article} /> : <BestFlex article={article} />}
     </div>
   );
 }
