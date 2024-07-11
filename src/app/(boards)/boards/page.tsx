71517d3b41f146d66fea9e5a0b007865c4ec5fde
eunbinnie <dmsqlsdl54@naver.com>
2024-07-11 23:22:23 +0900

    🔨refcator: 다시 클라이언트 컴포넌트로 변경 후 재렌더 테스트

--- src/app/(boards)/boards/page.tsx
+++ src/app/(boards)/boards/page.tsx
@@ -1,14 +1,17 @@
-import { Metadata } from "next";
+"use client";
+
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from "react";
 import getArticles from "@/apis/article/getArticles";
+import { ArticleData } from "@/types/articles.type";
 import BestPostList from "./components/bestArticles/BestPostList";
 import AllArticleSection, { ArticleOption } from "./components/allArticles/AllArticleSection";
 import BestHeader from "./components/bestArticles/BestHeader";
 
-export const metadata: Metadata = {
-  title: "WeKitBucket | 자유게시판",
-};
+function Board() {
+  const [allArticles, setAllArticles] = useState<ArticleData | null>(null);
+  const [bestArticles, setBestArticles] = useState<ArticleData | null>(null);
 
-async function Board() {
   const allArticlesOption: ArticleOption = {
     page: 1,
     pageSize: 10,
@@ -21,17 +24,24 @@ async function Board() {
     orderBy: "like",
   };
 
-  let allArticles;
-  let bestArticles;
+  useEffect(() => {
+    const fetchGetArticles = async () => {
+      try {
+        const all = await getArticles(allArticlesOption);
+        const best = await getArticles(bestArticlesOption);
+        setAllArticles(all);
+        setBestArticles(best);
+      } catch (error) {
+        console.error("Failed to fetch articles: ", error);
+      }
+    };
 
-  try {
-    [allArticles, bestArticles] = await Promise.all([getArticles(allArticlesOption), getArticles(bestArticlesOption)]);
-  } catch (error) {
-    console.error("Failed to fetch articles: ", error);
-  }
+    fetchGetArticles();
+  }, []);
 
   return (
     <div className="mx-auto mb-[57px] mt-10 grid gap-5">
+      <title>WeKitBucket | 자유게시판</title>
       <section className="grid gap-5">
         <BestHeader />
         {bestArticles && <BestPostList article={bestArticles} />}
