4bbadb0957515f0ffe1e19353e93525b9a5b8af8
yehaYoo <beauciel00@gmail.com>
2024-07-12 15:14:43 +0900

    🔥remove: 에디터 변경 전 코드 삭제

--- src/app/(boards)/addboard/[id]/page.tsx
+++ src/app/(boards)/addboard/[id]/page.tsx
@@ -1,23 +1,16 @@
 import React from "react";
 import { Metadata } from "next";
-import dynamic from "next/dynamic";
 import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "WeKitBucket | 게시물 수정하기",
 };
 
-const AddBoardComponent = dynamic(() => import("../AddBoardComponent"), {
-  ssr: false,
-});
-
-function AddBoardEditPage({ params }: { params: { id: string } }) {
-  const articleId = parseInt(params.id, 10);
-
+function AddBoardEditPage() {
   return (
     <div className="mx-[20px] mt-[32px] flex flex-col justify-center">
       <main className="md-shadow-custom-all mx-auto w-full gap-3 md:w-[624px] md:gap-5 md:rounded-[10px] md:border-gray-300 md:px-[30px] md:pb-[30px] md:pt-[40px] md:shadow-custom-all xl:w-[1060px]">
-        <AddBoardComponent articleId={articleId} />
+        {/* <AddBoardComponent articleId={articleId} /> */}
       </main>
       <div className="center flex justify-center">
         <Link
